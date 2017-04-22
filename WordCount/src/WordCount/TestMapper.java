87c149aa58ae3659fa87dd1e85e58487a47bd46c
aapeshave <peshave.aj@husky.neu.edu>
2017-04-21 21:03:24 -0400

    Adding word count program

---
+++ WordCount/src/WordCount/TestMapper.java
@@ -0,0 +1,22 @@
+package WordCount;
+
+import WordCount.pojo.Metadata;
+import com.google.gson.Gson;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+/**
+ * Created by ajinkya on 4/17/17.
+ */
+public class TestMapper extends Mapper<Object, Text, Text, NullWritable> {
+    @Override
+    protected void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+        Gson gson = new Gson();
+        Metadata metadata = gson.fromJson(value.toString(), Metadata.class);
+
+        context.write(new Text(metadata.getAsin()), NullWritable.get());
+    }
+}
---
+++ WordCount/src/WordCount/WordCount.java
@@ -0,0 +1,43 @@
+package WordCount;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Created by ajinkya on 4/21/17.
+ */
+public class WordCount {
+    public static void main(String args[]) {
+        Configuration configuration = new Configuration();
+        try {
+            Job job = Job.getInstance(configuration, "Project Test Data");
+            job.setJarByClass(WordCount.class);
+
+            job.setMapperClass(TestMapper.class);
+            job.setMapOutputKeyClass(Text.class);
+            job.setMapOutputValueClass(NullWritable.class);
+
+            job.setReducerClass(TestReducer.class);
+            job.setOutputKeyClass(Text.class);
+            job.setOutputValueClass(NullWritable.class);
+
+            FileInputFormat.addInputPath(job, new Path(args[0]));
+            FileOutputFormat.setOutputPath(job, new Path(args[1]));
+            System.exit(job.waitForCompletion(true) ? 0 : 1);
+
+        } catch (IOException | ClassNotFoundException ex) {
+            Logger.getLogger(WordCount.class.getName()).log(Level.SEVERE, ex.getMessage(), ex);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
